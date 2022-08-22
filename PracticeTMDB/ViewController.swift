e0377311ad3abe91910328394f57094ffd2bfe9e
김태현 <tason@tason-ui-Macmini.local>
2022-08-23 02:10:12 +0900

    (넷플릭스) 테이블/컬렉션뷰 복합활용연습 다시..

---
+++ PracticeTMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeTMDB
+//
+//  Created by 김태현 on 2022/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
